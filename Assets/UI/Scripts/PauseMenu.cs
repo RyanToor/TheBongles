880f096790bedf033db6a7a6fa7e2e9d8f69758d
Ryan Toor <ryantoor@hotmail.co.uk>
2021-11-13 15:43:33 +0000

    Example Panel Lerp Script
    Made CloseButton of PauseMenu a child of RightPanel.
    Disabled OpenButton and the animator component of RightPanel, so as not to interfere with script.
    Added OpenCloseMenu and Update methods along with associated variables to show how panel animation can be done with scripts.

--- Assets/UI/Scripts/PauseMenu.cs
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -6,39 +6,60 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
+    [Range(0, 1)]
+    public float lerpSpeed;
+    public Vector3 closedPos, openPos;
 
     public Animator rightPanel;
+
+    public float lerpPos = 0, lerpDir = -1;
+    private RectTransform rightPaneTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rightPaneTransform = transform.Find("RightPanel").GetComponent<RectTransform>();
+        rightPaneTransform.localPosition = closedPos;
     }
 
     // Update is called once per frame
-  /* void Update()
+    /* void Update()
+      {
+          if()
+          if (GameIsPaused)
+          {
+              Resume();
+          }
+          else
+          {
+              Pause();
+          }
+      }
+      public void Resume()
+      {
+          Time.timeScale = 1f;
+          GameIsPaused = false;
+      }
+
+      void Pause()
+      {
+          Time.timeScale = 0f;
+          GameIsPaused = true;
+      }*/
+
+    private void Update()
     {
-        if()
-        if (GameIsPaused)
+        if (lerpPos != Mathf.Clamp(lerpDir, 0, 1))
         {
-            Resume();
+            lerpPos = Mathf.Clamp(Mathf.Lerp(0, 1, lerpPos + lerpDir * lerpSpeed), 0, 1);
+            rightPaneTransform.localPosition = Vector3.Lerp(closedPos, openPos, lerpPos);
         }
-        else
-        {
-            Pause();
-        }
-    }
-    public void Resume()
-    {
-        Time.timeScale = 1f;
-        GameIsPaused = false;
     }
 
-    void Pause()
+    public void OpenCloseMenu()
     {
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-    }*/
+        lerpDir *= -1;
+    }
 
     public void LoadMenu()
     {
